ccde46ff61649fd7bd7ca4792b4bba2894b13ce8
Nicholas Michael Swoboda <nswobodastudent@shacademy.org>
2023-01-18 11:59:29 -0800

    working on boiler plate

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget Build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: '1-17 App',
+        theme: ThemeData(),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
